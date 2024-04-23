30d9e0b94c9ede1e97e5d7a84df9449a29819e33
Justin Xiao <jx16dev@gmail.com>
2024-04-23 00:38:18 -0700

    Display grid.

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 # Example file showing a basic pygame "game loop"
 import pygame
 
+
+# TODO: refactor to ROW, COL
+COL, ROW = 10, 20
+TILE_SIZE = 32
+RES = TILE_SIZE * COL, TILE_SIZE * ROW
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GRAY = (128, 128, 128)
+
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode(RES)
 clock = pygame.time.Clock()
 running = True
+
+# rect = pygame.Rect(1, 10, TILE_SIZE, TILE_SIZE)
+grid = [[pygame.Rect(c * TILE_SIZE, r * TILE_SIZE, TILE_SIZE, TILE_SIZE) for c in range(COL)] for r in range(ROW)]
 
 while running:
     # poll for events
@@ -18,6 +31,12 @@ while running:
     screen.fill("black")
 
     # RENDER YOUR GAME HERE
+    # pygame.draw.rect(screen, GRAY, rect)
+
+    for r in range(ROW):
+        for c in range(COL):
+            pygame.draw.rect(screen, GRAY, grid[r][c], 1)
+
 
     # flip() the display to put your work on screen
     pygame.display.flip()
