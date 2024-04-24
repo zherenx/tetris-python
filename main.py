76e4ab6b511362806d7d05cf037bbf56a270ed86
Justin Xiao <jx16dev@gmail.com>
2024-04-24 14:54:09 -0700

    Move game logic to Tetris class.

--- main.py
+++ main.py
@@ -19,57 +19,91 @@ running = True
 # rect = pygame.Rect(1, 10, TILE_SIZE, TILE_SIZE)
 grid = [[pygame.Rect(c * TILE_SIZE, r * TILE_SIZE, TILE_SIZE, TILE_SIZE) for c in range(COL)] for r in range(ROW)]
 
-tetrominoes = [
-    [
-        [0, 0, 0, 0],
-        [1, 1, 1, 1],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]
-    ], [
-        [0, 0, 0, 0],
-        [0, 1, 1, 0],
-        [0, 1, 1, 0],
-        [0, 0, 0, 0]
-    ], [
-        [0, 1, 0],
-        [1, 1, 1],
-        [0, 0, 0]
-    ], [
-        [1, 0, 0],
-        [1, 1, 1],
-        [0, 0, 0]
-    ], [
-        [0, 0, 1],
-        [1, 1, 1],
-        [0, 0, 0]
-    ], [
-        [0, 1, 1],
-        [1, 1, 0],
-        [0, 0, 0]
-    ], [
-        [1, 1, 0],
-        [0, 1, 1],
-        [0, 0, 0]
+class Tetris:
+
+    tetrominoes = [
+        [
+            [0, 0, 0, 0],
+            [1, 1, 1, 1],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ], [
+            [0, 0, 0, 0],
+            [0, 1, 1, 0],
+            [0, 1, 1, 0],
+            [0, 0, 0, 0]
+        ], [
+            [0, 1, 0],
+            [1, 1, 1],
+            [0, 0, 0]
+        ], [
+            [1, 0, 0],
+            [1, 1, 1],
+            [0, 0, 0]
+        ], [
+            [0, 0, 1],
+            [1, 1, 1],
+            [0, 0, 0]
+        ], [
+            [0, 1, 1],
+            [1, 1, 0],
+            [0, 0, 0]
+        ], [
+            [1, 1, 0],
+            [0, 1, 1],
+            [0, 0, 0]
+        ]
     ]
-]
 
-cur_tetromino = None
+    def __init__(self, h=20, w=10) -> None:
+        self.h = 20
+        self.w = 10
+        # self.gameboard = None
 
-def rotate(matrix):
-    transpose(matrix)
-    reflect(matrix)
+        self.cur_tetromino = None
+        self.tetromino_size = 0
+        self.x = 0
+        self.y = 0
+
+        self.init_tetromino()
     
-def transpose(matrix):
-    n = len(matrix)
-    for i in range(n):
-        for j in range(i + 1, n):
-            matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+    def init_tetromino(self):
+        # TODO: random
+        self.cur_tetromino = Tetris.tetrominoes[4]
+        self.tetromino_size = len(self.cur_tetromino)
+        self.x = (self.w - self.tetromino_size) // 2
+        self.y = 0
 
-def reflect(matrix):
-    n = len(matrix)
-    for i in range(n):
-        for j in range(n // 2):
-            matrix[i][j], matrix[i][-j - 1] = matrix[i][-j - 1], matrix[i][j]
+    def shift_horizontal(self, dx):
+        self.x += dx
+
+    def shift_down(self):
+        self.y += 1
+
+    def rotate(self):
+
+        def rotate(matrix):
+            transpose(matrix)
+            reflect(matrix)
+            
+        def transpose(matrix):
+            n = len(matrix)
+            for i in range(n):
+                for j in range(i + 1, n):
+                    matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+
+        def reflect(matrix):
+            n = len(matrix)
+            for i in range(n):
+                for j in range(n // 2):
+                    matrix[i][j], matrix[i][-j - 1] = matrix[i][-j - 1], matrix[i][j]
+        
+        if not self.cur_tetromino:
+            return
+
+        rotate(self.cur_tetromino)
+
+tetris_game = Tetris(ROW, COL)
 
 while running:
 
@@ -82,14 +116,13 @@ while running:
                 running = False
 
             if event.key == pygame.K_LEFT:
-                x -= 1
+                tetris_game.shift_horizontal(-1)
             elif event.key == pygame.K_RIGHT:
-                x += 1
+                tetris_game.shift_horizontal(1)
             elif event.key == pygame.K_DOWN:
-                y += 1
+                tetris_game.shift_down()
             elif event.key == pygame.K_UP:
-                if cur_tetromino:
-                    rotate(cur_tetromino)
+                tetris_game.rotate()
 
     screen.fill("black")
 
@@ -99,18 +132,18 @@ while running:
         for c in range(COL):
             pygame.draw.rect(screen, GRAY, grid[r][c], 1)
 
-    if not cur_tetromino:
-        # TODO: random
-        cur_tetromino = tetrominoes[4]
-        tetromino_size = len(cur_tetromino)
-        x, y = 3, 0
-
-    for r in range(tetromino_size):
-        for c in range(tetromino_size):
-            if cur_tetromino[r][c]:
-                pygame.draw.rect(
-                    screen, WHITE, pygame.Rect((x + c) * TILE_SIZE, (y + r) * TILE_SIZE, TILE_SIZE, TILE_SIZE)
-                )
+    if tetris_game.cur_tetromino:
+        for r in range(tetris_game.tetromino_size):
+            for c in range(tetris_game.tetromino_size):
+                if tetris_game.cur_tetromino[r][c]:
+                    pygame.draw.rect(
+                        screen, WHITE, 
+                        pygame.Rect(
+                            (tetris_game.x + c) * TILE_SIZE, 
+                            (tetris_game.y + r) * TILE_SIZE, 
+                            TILE_SIZE, TILE_SIZE
+                        )
+                    )
 
     pygame.display.flip()
 
